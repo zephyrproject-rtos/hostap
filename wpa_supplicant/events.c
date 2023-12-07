30b25044d70ff97049922633c4d3fa351ddf0895
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-12-07 21:05:23 +0530

    [toup] zephyr: Fix sending unsolicited disconnect result
    If STA is not connected but still disassociated for some reason e.g.,
    wrong password, sending disconnect results is not intuitive as we have
    never sent connected status up, so, instead just send connect result
    with the reason code for failure.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -3599,8 +3599,13 @@ static void wpa_supplicant_event_disassoc(struct wpa_supplicant *wpa_s,
 			MAC2STR(bssid), reason_code,
 			locally_generated ? " locally_generated=1" : "");
 #ifdef __ZEPHYR__
-		int status = 0;
-		supplicant_send_wifi_mgmt_event(wpa_s->ifname, NET_EVENT_WIFI_CMD_DISCONNECT_RESULT, (void *)&status, sizeof(int));
+		int status = reason_code;
+
+		if (wpa_s->wpa_state >= WPA_COMPLETED) {
+			supplicant_send_wifi_mgmt_event(wpa_s->ifname, NET_EVENT_WIFI_CMD_DISCONNECT_RESULT, (void *)&status, sizeof(int));
+		} else {
+			supplicant_send_wifi_mgmt_event(wpa_s->ifname, NET_EVENT_WIFI_CMD_CONNECT_RESULT, (void *)&status, sizeof(int));
+		}
 #endif /* __ZEPHYR__ */
 	}
 }
