ab5e1dab5f37c2b76b7c50a6c0585a4f3370f4e4
Fengming Ye <frank.ye@nxp.com>
2024-05-29 11:27:48 +0900

    [toup] zephyr: crypto: port crypto mbedtls wrapper
    Add crypto_mbedtls_alt.c and tls_mbedtls_alt.c,
    which have more functionality for enterprise and DPP.
    Can be used by crypto backend CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ALT.
    Add supp_psa_api.c to use PSA apis for HW acceleration in mbedtls 3.x.
    Add wpa_supp_els_pkc_mbedtls_config.h as an example for mbedtls
    user config for enterprise.
    
    Porting from
    https://github.com/gstrauss/hostap/blob/mbedtls/src/crypto/crypto_mbedtls.c
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

---
+++ port/mbedtls/supp_psa_api.h
@@ -0,0 +1,57 @@
+/** @file supp_psa_api.h
+ *
+ *  @brief  This file provides crypto mbedtls PSA APIs for wpa supplicant.
+ *
+ *  Copyright 2023 NXP
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef SUPP_PSA_API_H
+#define SUPP_PSA_API_H
+
+#include "includes.h"
+#include "common.h"
+
+#include "psa/crypto.h"
+#include "mbedtls/md.h"
+
+typedef enum
+{
+    SUPP_PSA_BLOCK_SIZE_128 = 16,
+    SUPP_PSA_BLOCK_SIZE_160 = 20,
+    SUPP_PSA_BLOCK_SIZE_192 = 24,
+    SUPP_PSA_BLOCK_SIZE_244 = 28,
+    SUPP_PSA_BLOCK_SIZE_256 = 32,
+    SUPP_PSA_BLOCK_SIZE_384 = 48,
+    SUPP_PSA_BLOCK_SIZE_512 = 64,
+} supp_psa_block_size_e;
+
+typedef enum
+{
+    SUPP_PSA_KEY_BITS_128 = 128,
+    SUPP_PSA_KEY_BITS_192 = 192,
+    SUPP_PSA_KEY_BITS_256 = 256,
+} supp_psa_key_bits_e;
+
+int aes_128_encrypt_block_psa(const u8 *key, const u8 *in, u8 *out);
+int aes_128_cbc_encrypt_psa(const u8 *key, const u8 *iv, u8 *data, size_t data_len);
+int aes_128_cbc_decrypt_psa(const u8 *key, const u8 *iv, u8 *data, size_t data_len);
+int aes_ctr_encrypt_psa(const u8 *key, size_t key_len, const u8 *nonce, u8 *data, size_t data_len);
+
+int omac1_aes_vector_psa(const u8 *key, size_t key_len, size_t num_elem, const u8 *addr[], const size_t *len, u8 *mac);
+
+int md_vector_psa(size_t num_elem, const u8 *addr[], const size_t *len, u8 *mac, mbedtls_md_type_t md_type);
+
+int hmac_vector_psa(const u8 *key,
+                    size_t key_len,
+                    size_t num_elem,
+                    const u8 *addr[],
+                    const size_t *len,
+                    u8 *mac,
+                    mbedtls_md_type_t md_type);
+
+int supp_psa_crypto_init(void);
+void supp_psa_crypto_deinit(void);
+#endif /* SUPP_PSA_API_H */
