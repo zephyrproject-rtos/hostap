3eab1e5b5541223ad73165ca6e306d6c1ef3b15a
Jouni Malinen <quic_jouni@quicinc.com>
2024-06-27 17:33:44 +0300

    [fromtree] Make driver capabilities for AKM suites available within wpa_supplicant
    In addition, add some of the previously missed AKM suites from the
    default capabilities.
    
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>
    (cherry picked from commit d0b55eb360d34231b095a14d55592ea682895e27)

--- wpa_supplicant/wpa_supplicant.c
+++ wpa_supplicant/wpa_supplicant.c
@@ -7127,6 +7127,7 @@ static int wpa_supplicant_init_iface(struct wpa_supplicant *wpa_s,
 		wpa_s->drv_flags = capa.flags;
 		wpa_s->drv_flags2 = capa.flags2;
 		wpa_s->drv_enc = capa.enc;
+		wpa_s->drv_key_mgmt = capa.key_mgmt;
 		wpa_s->drv_rrm_flags = capa.rrm_flags;
 		wpa_s->probe_resp_offloads = capa.probe_resp_offloads;
 		wpa_s->max_scan_ssids = capa.max_scan_ssids;
