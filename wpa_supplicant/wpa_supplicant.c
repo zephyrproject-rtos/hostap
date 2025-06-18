ajayparida/zephyrprojects-rtos_hostap

[noup] zephyr: Set max idle period
Dynamically set max idle period.

Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

wpa_supplicant/wpa_supplicant.c
@@ -4019,7 +4019,7 @@ static void wpas_start_assoc_cb(struct wpa_radio_work *work, int deinit)
 	params.sae_pwe = wpa_s->conf->sae_pwe;
 #endif /* CONFIG_SAE */
 
-	params.bss_max_idle_period = CONFIG_BSS_MAX_IDLE_TIME;
+	params.bss_max_idle_period = wpa_s->conf->max_idle_period;
 	ret = wpa_drv_associate(wpa_s, &params);
 	os_free(wpa_ie);
 	if (ret < 0) {

zephyrproject-rtos/hostap

wpa_supplicant/wpa_supplicant.c
@@ -4457,7 +4457,7 @@ static void wpas_start_assoc_cb(struct wpa_radio_work *work, int deinit)
 	params.sae_pwe = wpa_s->conf->sae_pwe;
 #endif /* CONFIG_SAE */
 
-	params.bss_max_idle_period = CONFIG_BSS_MAX_IDLE_TIME;
+	params.max_idle_period = wpa_s->conf->max_idle_period;
 	ret = wpa_drv_associate(wpa_s, &params);
 	os_free(wpa_ie);
 	if (ret < 0) {